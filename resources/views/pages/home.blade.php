555f87c69a7b79e4f3022110f51a44ce8c84d33f
syhruuu <muhammad90635@smk.belajar.id>
2023-12-08 08:55:31 +0700

    footer

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -27,15 +27,6 @@
 <div class="container text-center">
   <div class="row align-items-center">
     <div class="col">
-    <ul class="list-group">
-  <li class="list-group-item active" aria-current="true">Kategori</li>
-  <li class="list-group-item">Olahraga</li>
-  <li class="list-group-item">Politik</li>
-  <li class="list-group-item">Budaya</li>
-  <li class="list-group-item">Wisata</li>
-</ul>
-    </div>
-    <div class="col">
     <div class="container mt-3">
         <div class="row">
             @foreach ($allBerita as $item)
@@ -83,4 +74,43 @@
   </div>
 </div>
     </div>
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+    <div class="col mb-3">
+      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <p class="text-body-secondary">© 2023</p>
+    </div>
+
+    <div class="col mb-3">
+
+    </div>
+
+    <div class="col mb-3">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li>
+      </ul>
+    </div>
+  </footer>
 @endsection
